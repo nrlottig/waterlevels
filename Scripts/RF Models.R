37d6cc27066073b5e511b1446fa154cd0a29b34a
nrlottig <nrlottig@wisc.edu>
2017-11-03 15:22:31 -0500

    updating clustering

--- Scripts/RF Models.R
+++ Scripts/RF Models.R
@@ -37,50 +37,144 @@ table(waterlevelclusterID[,3])
 
 
 X = waterlevelclusterID[,c(7,13:180,182:183,185)]
-X = waterlevelclusterID[,c(7,13:24,29:80,85:155,160:180,182:183,185)] #remove runnoff
+X = waterlevelclusterID[,c(7,13:24,29:80,85:155,160:180,182:183,185:188)] #remove runnoff
+X2 = waterlevelclusterID[which(waterlevelclusterID$clusterid_dtw1_3!=1),c(7,13:24,29:80,85:155,160:180,182:183,185:188)]
 names(X)
 X[,160] = log10(X[,160])
-(rf.data = randomForest(y = Y,x = X,keep.inbag=TRUE,importance=TRUE,ntree=10001,sampsize=46))
+(rf.data = randomForest(y = Y,x = X,keep.inbag=TRUE,importance=TRUE,ntree=10001,sampsize=c(46,46,46),strata=Y))
 pred = predict(rf.data)
 (conf.out = confusionMatrix(pred,Y))
 
-med.vsurf = VSURF(x = X,y = Y,parallel = TRUE,ncores = 7)
+med.vsurf = VSURF(x = X,y = Y,parallel = TRUE,ncores = 7,ntree = 5001,nfor.thres = 5001,nfor.interp = 5001,nfor.pred = 5001,clusterType = "FORK",sampsize=c(46,46,46),strata=Y,keep.inbag=TRUE)
+med.vsurf = VSURF(x = X,y = Y,parallel = TRUE,ncores = 7,sampsize=c(46,46,46),strata=Y,clusterType = "FORK",keep.inbag=TRUE)
 names(X[med.vsurf$varselect.interp])
 names(X[med.vsurf$varselect.pred])
+# med.vsurf.dtw13.dmv13 = med.vsurf #Save output for future use 4.5 run time.
 X = X[,med.vsurf$varselect.pred]
-(rf.data2 = randomForest(y = Y,x = X,keep.inbag=TRUE,importance=TRUE,ntree=10001,sampsize=46))
+(rf.data2 = randomForest(y = Y,x = X,keep.inbag=TRUE,importance=TRUE,ntree=10001,sampsize=c(46,46,46),strata=Y))
 pred = predict(rf.data2)
 (conf.out = confusionMatrix(pred,Y))
 
-imp.out = importance(rf.data2)
-X = X[order(imp.out[,1],decreasing = TRUE)]
+(imp.out = importance(rf.data2,type=1,scale=FALSE))
+X = X[,c(2,1,3)]#X[order(imp.out[,1],decreasing = TRUE)]
 options(device="quartz")
 dev.new(width=6,height=4)
 par(oma=c(1.2,.8,.2,.2),mar=c(0,0,0,0),family="Times",ps=10)
-dotplot(imp.out[order(imp.out[,1],decreasing = FALSE),1],main="",xlab="Increase Mean Square Error (%)")
-
-ff = forestFloor(rf.fit=rf.data2,X=X,bootstrapFC = TRUE)
-ffcol=fcol(ff,cols=2)
-dev.new(width=9,height=4)
-par(oma=c(1.2,.8,.2,.2),mar=c(0,0,0,0),family="Times",ps=10,cex.axis=1.5,cex.lab=2,cex.main=1.5)
-plot(ff,orderByImportance=FALSE)
+good.anmes = c("Evergreen Forest (HU8,%)","Precipitation Cluster","Mean Annual Precip (30yr avg)")
+attributes(imp.out)$dimnames[[1]] = good.anmes
+dotplot(imp.out[order(imp.out[,1],decreasing = FALSE),1]*100,main="",xlab="Mean Decrease Accuracy (%)")
 
 names(X)
-plot.names = c("pct evergreen (HU8)","30yr avg precip (HU12)","Precip Cluster","30yr max precip (HU8)"
-               ,"pct evergreen (HU12)","min baseflow (HU12)","TRI (HU12)","Slope (HU12)")
-dev.new(width=8,height=4)
-par(mfrow=c(2,5),oma=c(0,0,0,0),mar=c(3,3,.2,.2),family="Times",ps=10,cex.axis=1,cex.lab=2,cex.main=1.5)
+plot.names = c("Evergreen Forest (HU8,%)","Precipitation Cluster","Mean Annual Precip (30yr avg)")
+dev.new(width=8,height=2)
+par(mfrow=c(1,3),oma=c(0,0,0,0),mar=c(3,3,.2,.2),family="Times",ps=10,cex.axis=1,cex.lab=2,cex.main=1.5)
 for(i in 1:ncol(X)){
   out1 = partialPlot(x = rf.data2,pred.data = X,x.var = names(X)[i],which.class = 1,plot=FALSE)
   out2 = partialPlot(x = rf.data2,pred.data = X,x.var = names(X)[i],which.class = 2,plot=FALSE)
   out3 = partialPlot(x = rf.data2,pred.data = X,x.var = names(X)[i],which.class = 3,plot=FALSE)
   yrange = range(out1$y,out2$y,out3$y)
-  plot(out1,col=1,type="l",ylim=yrange,xlab="",ylab="")
+  plot(out1,col=1,type="l",ylim=yrange,xlab="",ylab="",lwd=2)
   mtext(side=1,plot.names[i],line=1.8)
-  if(i==1 | i==5) mtext(side=2,"Relative logit contribution",line=1.8)
-  lines(out2,col=2,type="l")
-  lines(out3,col=3,type="l")
-  }
-out1 = partialPlot(x = rf.data2,pred.data = X,x.var = 1,which.class = 3)
+  if(i==1 | i==5) mtext(side=2,"Impact on Class Probability",line=1.8)
+  lines(out2,col=2,type="l",lwd=2)
+  lines(out3,col=3,type="l",lwd=2)
+}
 
-plot(waterlevelclusterID$Latitude,waterlevelclusterID$hu12_runoff_mean)
+
+##############
+##############
+##############
+# Run models without cluster 1 (southern WI)
+Y2 = waterlevelclusterID[[3]][which(waterlevelclusterID$clusterid_dtw1_3!=1)]
+Y2 = as.factor(as.numeric(Y))
+X2 = waterlevelclusterID[which(waterlevelclusterID$clusterid_dtw1_3!=1),c(7,13:24,29:80,85:155,160:180,182:183,185:188)]
+(rf.data = randomForest(y = Y2,x = X2,keep.inbag=TRUE,importance=TRUE,ntree=10001,sampsize=c(46,46),strata=Y))
+pred = predict(rf.data)
+(conf.out = confusionMatrix(pred,Y))
+varImpPlot(rf.data,scale=FALSE,type=1)
+
+med.vsurf = VSURF(x = X2,y = Y2,parallel = TRUE,ncores = 7,sampsize=c(46,46),strata=Y,clusterType = "FORK",keep.inbag=TRUE)
+names(X2[med.vsurf$varselect.interp])
+names(X2[med.vsurf$varselect.pred])
+X2 = X2[,med.vsurf$varselect.pred]
+(rf.data2 = randomForest(y = Y2,x = X,keep.inbag=TRUE,importance=TRUE,ntree=10001,sampsize=c(46,46),strata=Y))
+pred = predict(rf.data2)
+(conf.out = confusionMatrix(pred,Y2))
+varImpPlot(rf.data2,scale=FALSE,type=1)
+
+names(X2)
+
+plot.names = names(X2)#c("Evergreen Forest (HU8,%)","Precipitation Cluster","Mean Annual Precip (30yr avg)")
+dev.new(width=6,height=4)
+par(mfrow=c(2,3),oma=c(0,0,0,0),mar=c(3,3,.2,.2),family="Times",ps=10,cex.axis=1,cex.lab=2,cex.main=1.5)
+for(i in 1:ncol(X2)){
+  out1 = partialPlot(x = rf.data2,pred.data = X2,x.var = names(X2)[i],which.class = 1,plot=FALSE)
+  out2 = partialPlot(x = rf.data2,pred.data = X2,x.var = names(X2)[i],which.class = 2,plot=FALSE)
+  # out3 = partialPlot(x = rf.data2,pred.data = X2,x.var = names(X2)[i],which.class = 3,plot=FALSE)
+  yrange = range(out1$y,out2$y)
+  plot(out1,col=1,type="l",ylim=yrange,xlab="",ylab="",lwd=2)
+  mtext(side=1,plot.names[i],line=1.8)
+  if(i==1 | i==5) mtext(side=2,"Impact on Class Probability",line=1.8)
+  lines(out2,col=2,type="l",lwd=2)
+  # lines(out3,col=3,type="l",lwd=2)
+}
+
+
+#########RF Models without LTER Wells
+#set response variable
+
+factor.cols = c(3:7,10:12,184,185)
+
+#assigning factor to correct variables
+dataset = as.data.frame(dataset)
+for(i in 1:length(factor.cols)){
+  dataset[,factor.cols[i]] = as.factor(dataset[,factor.cols[i]]) 
+}
+
+
+Y = dataset[[189]]
+names(dataset)[189]
+#check counts for balancing RF model
+table(Y)
+
+
+X = dataset[,c(7,13:24,29:80,85:155,160:180,182:183,185:188)] #remove runnoff
+names(X)
+(rf.data = randomForest(y = Y,x = X,keep.inbag=TRUE,importance=TRUE,ntree=10001,sampsize=rep(min(table(Y)),nlevels(Y)),strata=Y))
+pred = predict(rf.data)
+(conf.out = confusionMatrix(pred,Y))
+
+med.vsurf = VSURF(x = X,y = Y,parallel = TRUE,ncores = 7,ntree = 5001,nfor.thres = 5001,nfor.interp = 5001,nfor.pred = 5001,clusterType = "FORK",sampsize=c(46,46,46),strata=Y,keep.inbag=TRUE)
+med.vsurf = VSURF(x = X,y = Y,parallel = TRUE,ncores = 7,sampsize=rep(min(table(Y)),nlevels(Y)),strata=Y,clusterType = "FORK",keep.inbag=TRUE)
+names(X[med.vsurf$varselect.interp])
+names(X[med.vsurf$varselect.pred])
+# med.vsurf.dtw13.dmv13 = med.vsurf #Save output for future use 4.5 run time.
+X = X[,med.vsurf$varselect.pred]
+(rf.data2 = randomForest(y = Y,x = X,keep.inbag=TRUE,importance=TRUE,ntree=10001,sampsize=rep(min(table(Y)),nlevels(Y)),strata=Y))
+pred = predict(rf.data2)
+(conf.out = confusionMatrix(pred,Y))
+
+(imp.out = importance(rf.data2,type=1,scale=FALSE))
+X = X[,c(2,1,3)]#X[order(imp.out[,1],decreasing = TRUE)]
+options(device="quartz")
+dev.new(width=6,height=4)
+par(oma=c(1.2,.8,.2,.2),mar=c(0,0,0,0),family="Times",ps=10)
+good.anmes = names(X)#c("Evergreen Forest (HU8,%)","Precipitation Cluster","Mean Annual Precip (30yr avg)")
+attributes(imp.out)$dimnames[[1]] = good.anmes
+dotplot(imp.out[order(imp.out[,1],decreasing = FALSE),1]*100,main="",xlab="Mean Decrease Accuracy (%)")
+
+names(X)
+plot.names = names(X)#c("Evergreen Forest (HU8,%)","Precipitation Cluster","Mean Annual Precip (30yr avg)")
+dev.new(width=8,height=4)
+par(mfrow=c(1,2),oma=c(0,0,0,0),mar=c(3,3,.2,.2),family="Times",ps=10,cex.axis=1,cex.lab=2,cex.main=1.5)
+for(i in 1:ncol(X)){
+  out1 = partialPlot(x = rf.data2,pred.data = X,x.var = names(X)[i],which.class = 1,plot=FALSE)
+  out2 = partialPlot(x = rf.data2,pred.data = X,x.var = names(X)[i],which.class = 2,plot=FALSE)
+  # out3 = partialPlot(x = rf.data2,pred.data = X,x.var = names(X)[i],which.class = 3,plot=FALSE)
+  yrange = range(out1$y,out2$y,out3$y)
+  plot(out1,col=1,type="l",ylim=yrange,xlab="",ylab="",lwd=2)
+  mtext(side=1,plot.names[i],line=1.8)
+  if(i==1 | i==5) mtext(side=2,"Impact on Class Probability",line=1.8)
+  lines(out2,col=2,type="l",lwd=2)
+  # lines(out3,col=3,type="l",lwd=2)
+}
--- Scripts/plots.R
+++ Scripts/plots.R
@@ -9,25 +9,21 @@ map('state',region=map.regions,col=grey(.98),fill=TRUE,resolution = 0,mar=c(0,0,
 points(waterlevelclusterID$Longitude,waterlevelclusterID$Latitude,col=alpha(as.numeric(waterlevelclusterID$clusterid_dtw1_3),.7),pch=16)
 legend("topright",legend=c(1:3),pch=16,col=c(1:3),title = "Cluster ID",bty="n")       
 
-map.regions = c('wisconsin')
-dev.new(width=5,height=5)
-map('state',region=map.regions,col=grey(.98),fill=TRUE,resolution = 0,mar=c(0,0,0,0),border=grey(.5),lty=5, main="Water Level Clusters")
-points(waterlevelclusterID$Longitude,waterlevelclusterID$Latitude,col=alpha(as.numeric(waterlevelclusterID$cmdv_clusterid3),.7),pch=16)
-legend("topright",legend=c(1:3),pch=16,col=c(1:3),title = "Cluster ID",bty="n") 
+ 
 
 rf.drivers = data.frame(wiscwaterlevelid=waterlevelclusterID$wiscwaterlevelid,clusterid = waterlevelclusterID$clusterid_dtw2_4,clusterpred=pred,meanrunnoff=waterlevelclusterID$hu12_runoff_mean,maxrunnoff=waterlevelclusterID$hu12_runoff_max,maxprecip=waterlevelclusterID$hu12_prism_ppt_30yr_normal_800mm2_annual_max)
 
 dev.new(width=4,height=4)
 par(oma=c(3,3,.2,.2),mar=c(0,0,0,0),family="Times",ps=10)
-boxplot(rf.drivers$meanrunnoff~rf.drivers$clusterid,xlab="",ylab="R")
+boxplot(waterlevelclusterID$hu8_nlcd2006_pct_42~waterlevelclusterID$clusterid_dtw1_3,xlab="",ylab="")
 mtext(side=1,line=2,"Cluster ID",cex=1.25)
-mtext(side=2,line=2,"Mean Runnoff (in/yr)",cex=1.25)
+mtext(side=2,line=2,"Evergreen Forest (HU8,%)",cex=1.25)
 
 dev.new(width=4,height=4)
 par(oma=c(3,3,.2,.2),mar=c(0,0,0,0),family="Times",ps=10)
-boxplot(rf.drivers$maxprecip~rf.drivers$clusterid,xlab="",ylab="")
+boxplot(waterlevelclusterID$hu12_prism_ppt_30yr_normal_800mm2_annual_mean~waterlevelclusterID$clusterid_dtw1_3,xlab="",ylab="")
 mtext(side=1,line=2,"Cluster ID",cex=1.25)
-mtext(side=2,line=2,"Max Precip (mm/yr)",cex=1.25)
+mtext(side=2,line=2,"Annual Precip (30yr avg,mm)",cex=1.25)
 
 dev.new(width=4,height=4)
 par(oma=c(3,3,.2,.2),mar=c(0,0,0,0),family="Times",ps=10)
