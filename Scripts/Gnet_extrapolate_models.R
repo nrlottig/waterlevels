0169160e07324d53b94d673efa6835f0d7987c6f
Noah Lottig <nrlottig@wisc.edu>
2018-11-16 18:46:38 -0600

    update extrap models

--- Scripts/Gnet_extrapolate_models.R
+++ Scripts/Gnet_extrapolate_models.R
@@ -4,7 +4,7 @@ library(foreach)
 library(doParallel)
 library(iterators)
 
-registerDoParallel(cores = 7)
+registerDoParallel(cores = 3)
 
 # ####The Data
 # Gnet_slopes <- read_csv("data/Gnet_slopes.csv") %>% select(WBIC)
@@ -22,12 +22,18 @@ registerDoParallel(cores = 7)
 gnet_met_data <- read_delim("big_data/gnet_met_data.csv", 
                             "\t", escape_double = FALSE, trim_ws = TRUE)
 gnet_mu_sims <- read_csv("data/gnet_mu_sims.csv")
+waterlevel <- read.csv("big_data/seepage_20180414.csv",sep=",",header=TRUE) %>% 
+  mutate(Date = as.Date(as.character(Date),format= "%m/%d/%Y")) %>% 
+  mutate(Value = as.numeric(as.character(Value))*304.8) %>% 
+  select(WBIC,Date,Value)
 
 #simmulations
 
 lakes = unique(gnet_met_data$WBIC)
 for(i in 1:length(lakes)) {
   dat <- gnet_met_data %>% filter(WBIC==1842400)
+  dat_level <- waterlevel %>% filter(WBIC==1842400) %>% 
+    filter(Value>50000)
   date_matrix = tibble(Date = seq(from=min(dat$Date),to=max(dat$Date),by="day"))
   dat <- date_matrix %>% left_join(dat)
   dat <- dat %>% mutate(Evap = replace_na(Evap, median(Evap,na.rm=TRUE) )) %>% 
@@ -44,11 +50,18 @@ for(i in 1:length(lakes)) {
     S[1,] = 100*1000
     for(z in 2:nrow(S)){
       sims <- sample_n(gnet_mu_sims,1)
-      S[z,1] <- S[(z-1),1] + sims[[1,2]]*(dat$Precip[z] + dat$Evap[z]) + (-9.100742e-5*(S[(z-1),1]-99276.71) + sims[[1,1]])
+      S[z,1] <- S[(z-1),1] + sims[[1,2]]*(dat$Precip[z] + dat$Evap[z]) - (.000024*(S[(z-1),1]) + sims[[1,1]])
     } #end of time series loop
-    S
-    plot(dat$Date,S[,1]/1000,type="l")
-  } #end 1000 simulation loop
+    # S
+    extrap_level <- data.frame(Date= dat$Date, level = S)
+    dat.compare = dat_level %>% left_join(extrap_level) %>% 
+      mutate(offset = level-Value)
+    offset <- mean(dat.compare$offset)
+    ggplot(data = extrap_level,aes(x=Date,y=level)) + 
+      geom_line(data=dat_level,aes(x=Date,y=(Value + offset)),color="red") + 
+      geom_line()
+      
+} #end 1000 simulation loop
   out <- as.data.frame(t(apply(out_loop, 1, quantile, c(0.025,0.5,0.975))))
   out <- cbind(dat$WBIC,dat$Date,out)
   names(out) <- c("WBIC","Date","ll_val","median_val","ul_val")
